14b2d6fe9c7b5018c4b5c922b1700c7e5beac869
Username <98346368+JoeHaney@users.noreply.github.com>
2022-03-10 11:09:21 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    . . . . 2 2 2 2 2 . . . . . . . 
+    . . . 2 2 2 2 2 2 2 2 2 . . . . 
+    . . . e e e d d f d . . . . . . 
+    . . e d e d d d f d d d . . . . 
+    . . e d e e d d d f d d d . . . 
+    . . e e d d d d f f f f . . . . 
+    . . . . d d d d d d d . . . . . 
+    . . . 2 2 8 2 2 2 . . . . . . . 
+    . . 2 2 2 8 2 2 8 2 2 2 . . . . 
+    . 2 2 2 2 8 8 8 8 2 2 2 2 . . . 
+    . d d 2 8 5 8 8 5 8 2 d d . . . 
+    . d d d 8 8 8 8 8 8 d d d . . . 
+    . d d 8 8 8 8 8 8 8 8 d d . . . 
+    . . . 8 8 8 . . 8 8 8 . . . . . 
+    . . e e e . . . . e e e . . . . 
+    . e e e e . . . . e e e e . . . 
+    `, SpriteKind.Player)
